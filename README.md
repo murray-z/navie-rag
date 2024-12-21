4b5281c479eb86b1b3576006157e1c51a3dc2a26
zhangfazhan <1650011565@qq.com>
2024-12-21 21:35:56 +0800

    add message in readme and config

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 - 使用LangChain库实现文本分割
 - 使用Milvus库实现向量检索
 
+# 使用
+- 下载ollama，部署llm和embedding模型
+- 修改配置文件config.py
+- 运行main.py
+
 # TODO
 - [ ] Reranker模型
--- config.py
+++ config.py
@@ -1,9 +1,16 @@
+# ollama
 EMBEDDING_MODEL_NAME = "bge-large:latest"
 LLM_MODEL_NAME = "qwen2.5:0.5b"
+
+# milvus
 MILVUS_COLLECTION_NAME = "navie_rag"
 MILVUS_HOST = "./milvus_db/navie_rag.db"
+
+# chunk
 CHUNK_SIZE=500
 OVERLAP_SIZE=100
+
+# search
 SCORE_THRESHOLD = 0.3
 RAG_CHAT_TEMPLATE = """你是一个人工智能助手，你的任务是根据参考信息，回答用户的问题。
 如果参考信息为空或者答案不在参考信息中，请回答‘对不起，无法给出准确答案。’
